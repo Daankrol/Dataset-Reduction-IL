748d10f54bb457dcb9b85b4a57b56d6328c6d9dd
Sahasra Ranjan <sahasraranjan@gmail.com>
2022-04-12 09:48:37 +0530

    updated docs

--- cords/utils/data/dataloader/SL/adaptive/selcondataloader.py
+++ cords/utils/data/dataloader/SL/adaptive/selcondataloader.py
@@ -5,9 +5,12 @@ import time, copy
 
 # SELCONstrategy
 class SELCONDataLoader(AdaptiveDSSDataLoader):
+    """
+    Implementation of SELCONdataloader that serves as the dataloader for SELCON algorithm.
+    """
     def __init__(self, trainset, validset, train_loader, val_loader, dss_args, logger, *args, **kwargs):
         """
-         Arguments assertion check
+        Constructor function
         """
         assert "model" in dss_args.keys(), "'model' is a compulsory argument. Include it as a key in dss_args"
         assert "loss" in dss_args.keys(), "'loss' is a compulsory argument. Include it as a key in dss_args"
