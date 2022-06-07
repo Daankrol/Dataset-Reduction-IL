6774738a94db9c935eb6efb0c7eb5d42e2bc8154
Daan Krol <daankrol@icloud.com>
2022-06-07 18:27:31 +0200

    removed workflows and added test slurm script

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from train_sl import TrainClassifier
 from cords.utils.config_utils import load_config_data
 
 print("test!")
-config_file = "./configs/SL/config_glister_cifar10.py"
+config_file = "./configs/SL/config_gradmatch_cifar10.py"
 cfg = load_config_data(config_file)
 print(cfg)
 clf = TrainClassifier(cfg)
