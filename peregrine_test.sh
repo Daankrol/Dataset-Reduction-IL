589920e9126433d1b4a551482ce903fd1636fcc0
Daan Krol <daankrol@icloud.com>
2022-06-08 12:56:58 +0200

    using gpu-short for tests

--- peregrine_test.sh
+++ peregrine_test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --time=3:00:00
+#SBATCH --time=00m:59:00
 #SBATCH --job-name=peregrine_test
-#SBATCH --mem=40G
+#SBATCH --mem=100G
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE
 #SBATCH --mail-user=d.j.krol.1@student.rug.nl
 #SBATCH --array=1
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:k40:1
+#SBATCH --partition=gpu-short
+#SBATCH --gres=gpu:v100:1
 
 
 module purge
-module load PyTorch/1.10.0-fosscuda-2020b
+# module load PyTorch/1.10.0-fosscuda-2020b
+module load Python/3.8.6-GCCcore-10.2.0
 # On peregrine use the old dependency resolver of pip:
 # python -m pip install --use-feature=2020-resolver -r requirements/requirements.txt
-# module load Python/3.8.6-GCCcore-10.2.0
 
 source /data/$USER/.envs/DatasetReduction/bin/activate
 
