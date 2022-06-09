64d536f6db28be91b47f62e0f2a0f44ee9408135
Daan Krol <daankrol@icloud.com>
2022-06-09 15:19:38 +0200

    using logging for slurm files

--- getSLURMenv.py
+++ getSLURMenv.py
@@ -1,22 +1,25 @@
-# Load and print the contents of the env variable SBATCH_CPU_IDS, SBATCH_GPU_IDS and SBATCH_CPU_BIND_LIST
-#
-import os
+import logging
+
+def get_cpu_ids():
+    return os.environ.get('SBATCH_CPU_IDS')
 
 
-# get the cpu ids and gpu ids from the environment variables
-# def get_cpu_gpu_ids(env_var_name):
-#     cpu_gpu_ids = os.environ.get(env_var_name)
-#     if cpu_gpu_ids is None:
-#         print(f"Error: environment variable {env_var_name} is not set")
-#         sys.exit(1)
-#     # split the string and get the cpu ids
-#     cpu_gpu_ids = cpu_gpu_ids.split(",")
-#     cpu_gpu_ids = [i.split("-") for i in cpu_gpu_ids]
-#     cpu_gpu_ids = [
-#         [int(i) for i in cpu_gpu_ids[0]] + [int(i) for i in cpu_gpu_ids[1]]
-#     ]
-#     return cpu_gpu_ids
+def get_gpu_ids():
+    return os.environ.get('SBATCH_GPU_IDS')
 
-print('cpu ids:', os.environ.get('SBATCH_CPU_IDS'))
-print('gpu ids:', os.environ.get('SBATCH_GPU_IDS'))
-print('cpu bind list:', os.environ.get('SBATCH_CPU_BIND_LIST'))
+
+def get_cpu_bind_list():
+    return os.environ.get('SBATCH_CPU_BIND_LIST')
+
+
+def get_gpu_bind_list():
+    return os.environ.get('SBATCH_GPU_BIND_LIST')
+
+
+# now call all functions and log it in a file 
+logging.basicConfig(filename='slurm_envs.log', level=logging.INFO)
+logging.info(f'cpu ids: {get_cpu_ids()}')
+logging.info(f'gpu ids: {get_gpu_ids()}')
+logging.info(f'cpu bind list: {get_cpu_bind_list()}')
+logging.info(f'gpu bind list: {get_gpu_bind_list()}')
+
