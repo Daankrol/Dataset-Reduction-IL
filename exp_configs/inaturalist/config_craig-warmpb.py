efc1b957b20d87828a76201bc75a78d6c7ae7236
Daan Krol <daankrol@icloud.com>
2022-07-14 18:01:59 +0200

    added configs for inaturalist set

---
+++ exp_configs/inaturalist/config_craig-warmpb.py
@@ -0,0 +1,46 @@
+# Learning setting
+config = dict(
+    setting="SL",
+    measure_energy=True,
+    logging='DEBUG',
+    wandb=True,
+    is_reg=False,
+    dataset=dict(name="inaturalist", datadir="../data", feature="dss", type="image", img_size=224),
+    dataloader=dict(shuffle=True, batch_size=20, pin_memory=True),
+    model=dict(architecture="EfficientNet", type="pre-defined"),
+    ckpt=dict(is_load=False, is_save=False, dir="results/", save_every=20),
+    loss=dict(type="CrossEntropyLoss", use_sigmoid=False),
+    optimizer=dict(
+        type="sgd", momentum=0.9, lr=0.005, weight_decay=5e-4, nesterov=False
+    ),
+    scheduler=dict(type="cosine_annealing", T_max=300),
+    dss_args=dict(
+        type="CRAIGPB-Warm",
+        fraction=0.2,
+        select_every=10,
+        kappa=0.05,
+        linear_layer=False,
+        optimizer="lazy",
+        selection_type="PerBatch",
+        if_convex=False,
+    ),
+    train_args=dict(
+        num_epochs=300,
+        device="cuda",
+        print_every=1,
+        results_dir="../results/",
+        print_args=[
+            "val_loss",
+            "val_acc",
+            "tst_loss",
+            "tst_acc",
+            "trn_loss",
+            "trn_acc",
+            "trn_recall",
+            "tst_recall",
+            "val_recall",
+            "time",
+        ],
+        return_args=[],
+    ),
+)
---
+++ exp_configs/inaturalist/config_gradmatchpb-warm.py
@@ -0,0 +1,48 @@
+# Learning setting
+config = dict(
+    setting="SL",
+    measure_energy=True,
+    logging='DEBUG',
+    wandb=True,
+    is_reg=False,
+    dataset=dict(name="inaturalist", datadir="../data", feature="dss", type="image", img_size=224),
+    dataloader=dict(shuffle=True, batch_size=20, pin_memory=True),
+    model=dict(architecture="EfficientNet", type="pre-defined"),
+    ckpt=dict(is_load=False, is_save=False, dir="results/", save_every=20),
+    loss=dict(type="CrossEntropyLoss", use_sigmoid=False),
+    optimizer=dict(
+        type="sgd", momentum=0.9, lr=0.005, weight_decay=5e-4, nesterov=False
+    ),
+    scheduler=dict(type="cosine_annealing", T_max=300),
+    dss_args=dict(
+        type="GradMatchPB-Warm",
+        fraction=0.2,
+        select_every=10,
+        lam=0.5,
+        selection_type="PerBatch",
+        v1=True,
+        valid=False,
+        eps=1e-100,
+        linear_layer=True,
+        kappa=0.05,
+    ),
+    train_args=dict(
+        num_epochs=300,
+        device="cuda",
+        print_every=1,
+        results_dir="../results/",
+        print_args=[
+            "val_loss",
+            "val_acc",
+            "tst_loss",
+            "tst_acc",
+            "trn_loss",
+            "trn_acc",
+            "time",
+            "trn_recall",
+            "tst_recall",
+            "val_recall",
+        ],
+        return_args=[],
+    ),
+)
